62c7b7c25e916af04c19ec24de5cb07125f08fdd
Kevin Gao <sudowork@gmail.com>
2013-04-06 14:51:28 -0400

    [extension] Added initial skeleton for chrome extension

---
+++ extension/hover-extension/background.js
@@ -0,0 +1,10 @@
+chrome.browserAction.onClicked.addListener( function() {
+    chrome.tabs.executeScript( null, {
+        file: 'socket.io.min.js'
+    }, function() {
+        chrome.tabs.executeScript( null, {
+            file: 'hover.js'
+        });
+    });
+});
+
---
+++ extension/hover-extension/hover.js
@@ -0,0 +1,3 @@
+(function(){
+  console.log('Hover extension loaded');
+})();
